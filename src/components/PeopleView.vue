06b4270d36efdb52fb4e72b254f6dbe0864b22d5
17030752 <davidenriqueprogramacion@gmail.com>
2022-08-05 12:50:35 -0500

    example of split-bill with vue

---
+++ src/components/PeopleView.vue
@@ -0,0 +1,66 @@
+<script setup>
+	import {store,getGrandTotal} from '../store/store.js';
+	import Person from "../components/Person.vue";
+	import Label from "../components/Label.vue";
+</script>
+
+<template>
+			<div class ="no-items" v-if="store.people.length==0">
+				no-items
+			</div>
+			<div class="people-view" v-if="store.people.length > 0">
+				<div clas="header">
+					<div>
+						<Label title="Total + Tip:" :value="getGrandTotal()"/>
+					</div>
+					<div>
+						<Label title="Remaining:" :value="store.params.remaining"/>
+					</div>
+				</div>
+				<div class ="people-container">
+					
+						<Person 
+						v-for="person in store.people" :key="person.id"
+						:id="person.id" 
+						:number-of-person="person.numberOfPerson" 
+						:total-per-person="person.totalPerPerson" 
+						:paid="person.paid"
+						/>
+				</div>
+			</div>
+</template>
+
+<style scoped>
+.no-items{
+	width:100%;
+	height:100vh;
+	display:flex;
+	align-items:center;
+	justify-content:center;
+	font-size:28px;
+}
+.people-view{
+margin:0 auto;
+display:flex;
+align-items:center;
+flex-direction:column;
+}
+.people-container{
+	width:800px;
+	margin:0 auto;
+	display:grid;
+	grid-template-columns: 1fr 1fr 1fr;
+	grid-template-rows:auto;
+	gap:10px;
+}
+.header{
+	color:white;
+	font-weight:bolder;
+	padding:20px 0;
+	font-size:28px;
+	display:flex;
+	jsutify-content:space-between;
+	gap:10px;
+	flex-direction:column;
+}
+</style>
---
+++ src/components/SplitBillView.vue
@@ -0,0 +1,17 @@
+<script setup>
+	import InputForm from "../components/InputForm.vue";
+	import PeopleView from "../components/PeopleView.vue"
+</script>
+
+<template>
+	<div class="container">
+		<InputForm/>
+		<PeopleView/>
+	</div>
+</template>
+
+<style scoped>
+.container{
+	display:flex;
+}
+</style>
